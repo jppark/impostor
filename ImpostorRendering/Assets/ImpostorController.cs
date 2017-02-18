6e20d2f60a5a7904e915266363f5d9360dcccaa9
jppark <jpster99@gmail.com>
2017-02-18 22:06:46 +0900

    Test Commit

--- ImpostorRendering/Assets/ImpostorController.cs
+++ ImpostorRendering/Assets/ImpostorController.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class ImpostorController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
